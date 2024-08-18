ef84730b8b5efdc45403f9ceca3144287db97714
Jaden Down <24631328+jpdown@users.noreply.github.com>
2024-08-17 20:37:28 -0500

    receiving song title upon change

--- universal-windows-scrobbler/ViewModels/MainWindowViewModel.cs
+++ universal-windows-scrobbler/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,27 @@
-﻿namespace universal_windows_scrobbler.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Tmds.DBus.Protocol;
+using universal_windows_scrobbler.Models;
+
+namespace universal_windows_scrobbler.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    [ObservableProperty] private string _title = "No Song Playing";
+    
+    private MediaSession _mediaSession; 
+    
+    public MainWindowViewModel()
+    {
+        _mediaSession = new MediaSession();
+        _mediaSession.SongTitleChanged += MediaSessionOnSongTitleChanged;
+    }
+
+    private void MediaSessionOnSongTitleChanged(MediaSession sender, string title)
+    {
+        Title = title;
+    }
 }
